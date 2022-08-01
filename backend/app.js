070fed4774551ada4b00450701368cfb09e681fa
m4d <julien.conforto@gmail.com>
2022-08-01 08:52:33 +0200

    mongoose init

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    'mongodb+srv://mad7444:<password>@p6cluster.e4vqd.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
   res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
 });
 
 module.exports = app;
